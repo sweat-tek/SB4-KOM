f12b42eb2926d4dacb6d9d6654baca70415fe73b
corfixen <jcs@mmmi.sdu.dk>
2020-02-10 12:28:41 +0100

    AsteroidsEntityFramework

---
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -0,0 +1,9 @@
+package dk.sdu.mmmi.cbse.common.services;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+
+public interface IEntityProcessingService {
+
+    void process(GameData gameData, World world);
+}
