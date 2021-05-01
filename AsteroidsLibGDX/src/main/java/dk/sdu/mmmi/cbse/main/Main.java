1ccb3e0b65ab288a678350784e64df7f9571004d
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-05-01 22:00:10 +0200

    Fix creation of executable jar

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -4,19 +4,18 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		LwjglApplicationConfiguration cfg =
-			new LwjglApplicationConfiguration();
-		cfg.title = "Asteroids";
-		cfg.width = 500;
-		cfg.height = 400;
-		cfg.useGL30 = false;
-		cfg.resizable = false;
-		
-		new LwjglApplication(new Game(), cfg);
-		
-	}
-	
+
+    public static void main(String[] args) {
+
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Asteroids";
+        cfg.width = 500;
+        cfg.height = 400;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(new Game(), cfg);
+
+    }
+
 }
