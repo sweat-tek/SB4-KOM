1f0a5a43b058e58ba74491997928646acd2ad898
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-05-01 23:13:51 +0200

    :rotating_light: Fix all linterwarning

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -5,17 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Asteroids";
-        cfg.width = 500;
-        cfg.height = 400;
-        cfg.useGL30 = false;
-        cfg.resizable = false;
+    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+    cfg.title = "Asteroids";
+    cfg.width = 500;
+    cfg.height = 400;
+    cfg.useGL30 = false;
+    cfg.resizable = false;
 
-        new LwjglApplication(new Game(), cfg);
-
-    }
-
+    new LwjglApplication(new Game(), cfg);
+  }
 }
