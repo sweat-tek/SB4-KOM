e97ac0c2bc4be671552b4e15addef27001f9ea30
Michael Gertz <michael@ringhus.dk>
2023-03-01 10:49:31 +0100

    Removed Asteroids

--- Asteroids/Common/src/main/java/dk/sdu/student/miger20/common/services/IEntityProcessingService.java
+++
@@ -1,9 +0,0 @@
-package dk.sdu.student.miger20.common.services;
-
-import dk.sdu.student.miger20.common.data.GameData;
-import dk.sdu.student.miger20.common.data.World;
-
-public interface IEntityProcessingService {
-
-    void process(GameData gameData, World world);
-}
