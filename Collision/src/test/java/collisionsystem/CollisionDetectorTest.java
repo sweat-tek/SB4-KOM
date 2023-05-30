9de0e80bb2c984a1926764d03bdde737f68234d6
OFHansen <ohans21@student.sdu.dk>
2023-05-31 00:07:28 +0200

    added JUnit to pom xml

---
+++ Collision/src/test/java/collisionsystem/CollisionDetectorTest.java
@@ -0,0 +1,14 @@
+package collisionsystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollisionDetectorTest {
+
+    @Test
+    void thisIsTrue(){
+        assertTrue(true);
+    }
+
+}
