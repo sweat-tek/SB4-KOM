52623bd1c2a61cca46de1893984abcea19017952
OFHansen <ohans21@student.sdu.dk>
2023-05-30 21:16:31 +0200

    made enemy less trigger happy

--- Core/src/main/java/core/main/Main.java
+++ Core/src/main/java/core/main/Main.java
@@ -10,10 +10,10 @@ public class Main {
 		Lwjgl3ApplicationConfiguration cfg =
 				new Lwjgl3ApplicationConfiguration();
 		cfg.setTitle("Asteroids");
-		cfg.setWindowedMode(500, 400);
+		cfg.setWindowedMode(1000, 800);
 		cfg.setResizable(false);
 		new Lwjgl3Application(new Game(), cfg);
-		
+
 	}
 	
 }
